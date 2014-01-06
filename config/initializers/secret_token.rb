93a2accfcb45ee91dd7014e39113bd97ca25df38
Tyler Boyd <tyler@epicpromo.ca>
2014-01-05 20:50:58 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MasicServer::Application.config.secret_key_base = 'f0c9848b80bb652de9f80d1db7638ed60f15588dc12a5bb3db85c490d8fe90a97e996146f87485d2882b46323fafbd0dedb3f48255327d0ea1670bee46a8fd98'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MasicServer::Application.config.session_store :cookie_store, key: '_masic-server_session'
